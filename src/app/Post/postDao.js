a2d3a6301d493bed7a45ae61e9af1b2f974b047e
Kid-Chang <pch3310@gmail.com>
2023-03-02 15:22:49 +0900

    feat(Post): 포스트 및 포스트 이미지 업로드 구현

--- src/app/Post/postDao.js
+++ src/app/Post/postDao.js
@@ -17,6 +17,56 @@ async function selectuserPosts(connection, userIdx) {
   return userPostsRows;
 }
 
+// 게시물 정보 조회
+async function selectPosts(connection, userIdx) {
+  const selectPostsQuery = `
+    SELECT p.postIdx as postIdx,
+    u.userIdx as userIdx,
+    u.nickName as nickName,
+    u.profileImgUrl as profileImgUrl,
+    p.content as content,
+    
+    IF(commentCount is null, 0, commentCount) as commentCount,
+    case
+        when timestampdiff(second, p.updatedAt, current_timestamp) < 60
+            then concat(timestampdiff(second, p.updatedAt, current_timestamp), '초 전')
+        when timestampdiff(minute , p.updatedAt, current_timestamp) < 60
+            then concat(timestampdiff(minute, p.updatedAt, current_timestamp), '분 전')
+        when timestampdiff(hour , p.updatedAt, current_timestamp) < 24
+            then concat(timestampdiff(hour, p.updatedAt, current_timestamp), '시간 전')
+        when timestampdiff(day , p.updatedAt, current_timestamp) < 365
+            then concat(timestampdiff(day, p.updatedAt, current_timestamp), '일 전')
+        else timestampdiff(year , p.updatedAt, current_timestamp)
+    end as uploadTime
+    
+    FROM Post as p
+        join User as u on u.userIdx = p.userIdx
+        left join (select postIdx, count(commentIdx) as commentCount from Comment WHERE status = 'ACTIVE' group by postIdx) c on c.postIdx = p.postIdx
+        left join Follow as f on f.followeeIdx = p.userIdx and f.status = 'ACTIVE'
+        
+    WHERE f.followerIdx = ? and p.status = 'ACTIVE'
+    group by p.postIdx
+    `;
+
+  const [postRows] = await connection.query(selectPostsQuery, userIdx);
+  return postRows;
+}
+
+async function selectPostImgs(connection, postIdx) {
+  const selectPostImgsQuery = `
+    select pi.postImgUrlIdx,
+        pi.imgUrl
+        FROM PostImgUrl as pi
+            join Post as p on p.postIdx = pi.postIdx
+        WHERE pi.status = 'ACTIVE' and p.postIdx = ?
+        `;
+
+  const [postImgsRows] = await connection.query(selectPostImgsQuery, postIdx);
+  return postImgsRows;
+}
+
 module.exports = {
   selectuserPosts,
+  selectPosts,
+  selectPostImgs,
 };
--- src/app/Post/postProvider.js
+++ src/app/Post/postProvider.js
@@ -9,3 +9,19 @@ exports.retrieveUserPosts = async function (userIdx) {
 
   return userPostsResult;
 };
+
+exports.retrievePostList = async function (userIdx) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const postListResult = await postDao.selectPosts(connection, userIdx);
+
+  // 여기서 forEach 쓰면 안됨. forEach는 뿌려주는 거고, for of는 하나씩 잠깐 찢어서 빌려주는거. 즉, post.imgs라고 작성하는 부분들이 postListResult에 반영됨.
+  for (post of postListResult) {
+    const postIdx = post.postIdx;
+    const postImgResult = await postDao.selectPostImgs(connection, postIdx);
+    post.imgs = postImgResult;
+  }
+
+  connection.release();
+
+  return postListResult;
+};
